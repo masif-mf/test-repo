f49b42fe1d97420c4795524e5bd337d3b666da7f
Muhammad.Asif.25165 <Muhammad.Asif@microfocus.com>
2022-01-05 16:11:38 +0000

    testing

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ module "test" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-security-group.git?ref=v3.14.0"
   create = false
 }
+
+
+module "test" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-security-group.git?ref=v3.15.0"
+  create = false
+}
